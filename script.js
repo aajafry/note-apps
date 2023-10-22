087a7a1c399595adc5ca2bb1ff63dbc58d77af72
aajafry <aajafry2@gmail.com>
2023-10-22 17:40:09 +0600

    javascript modification

--- script.js
+++ script.js
@@ -1,96 +1,87 @@
 // select elements & assign them to variables.
-let formInfo = document.querySelector("note-apps-from");
-let getFormTitle = document.querySelector(".note-heading-input");
-let getFormText = document.querySelector(".note-paragraph-textarea");
-let formSaveBtn = document.querySelector(".from-save-btn");
+// const formInfo = document.querySelector(".note-apps-from");
+const getFormTitle = document.querySelector(".note-heading-input");
+const getFormText = document.querySelector(".note-paragraph-textarea");
+const formSaveBtn = document.querySelector(".from-save-btn");
 
-let formBtnGroup = document.querySelector(".form-btn-group");
-let formUpdateBtn = document.querySelector(".from-update-btn");
-let formDeleteBtn = document.querySelector(".from-clear-btn");
+const formBtnGroup = document.querySelector(".form-btn-group");
+const formUpdateBtn = document.querySelector(".from-update-btn");
+const formResetBtn = document.querySelector(".from-clear-btn");
 
-let notesWrapper = document.querySelector(".notes-contents-wrapper");
-let noteContentBody = document.querySelector(".note-content-body");
-let closeBtn = document.querySelector(".close-btn");
-let noteContent = document.querySelector(".note-content");
-let noteHeading = document.querySelector(".note-heading");
-let noteParagraph = document.querySelector(".note-paragraph");
+const notesWrapper = document.querySelector(".notes-contents-wrapper");
+// const noteContentBody = document.querySelector(".note-content-body");
+// const closeBtn = document.querySelector(".close-btn");
+const noteContent = document.querySelector(".note-content");
+const noteHeading = document.querySelector(".note-heading");
+// const noteParagraph = document.querySelector(".note-paragraph");
 
 // store all note into notes array.
 const notes = JSON.parse(localStorage.getItem("notes")) || [];
 
 // create note html template function.
-let createNotetemp = (note) => {
+const createNotetemp = (note) => {
     return `<div class="note-content-body">
-        <button class="close-btn">Close</button>
-        <div class="note-content">
+        <button 
+            class="close-btn" 
+            id="close-btn" 
+            type="submit" 
+            onclick="closeNote(this)"
+        >Close</button>
+        <div class="note-content" onclick="dataPassToForm(this)">
             <h2 class="note-heading">${note.title}</h2>
             <p class="note-paragraph">${note.text}</p>
         </div>
     </div>`;
 }
-
-// add note function.
-let addNote = (title,text) => {
-    //  note object declaretion.
-    let createNote = {
-        title: title,
-        text: text
-    }
-    notes.push(createNote); // adding new note into notes array.
-    localStorage.setItem("notes",JSON.stringify(notes));  // store notes into local storage.
-}
-
 // save event listener for adding note.
 formSaveBtn.addEventListener("click", (event) => {
     event.preventDefault();
+    const createNote = {
+        title: getFormTitle.value,
+        text:  getFormText.value
+    }
     notesWrapper.innerHTML += createNotetemp(createNote); // add new note temp. into note-wrapper div.
-    // invoke addnote function.
-    addNote(getFormTitle.value,getFormText.value);
-    event.reset; // reset the form input data.
-})
-
-// reset even listener for form clear.
-formDeleteBtn.addEventListener("click", (event) => {
-    event.preventDefault();
+    notes.push(createNote); // adding new note into notes array.
+    localStorage.setItem("notes",JSON.stringify(notes));  // store notes into local storage.
+    // reset the form input data.
     getFormTitle.value = "";
     getFormText.value =  "";
 })
-
-// close event listener for note delete.
-closeBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    event.target.parentElement.remove();
-})
 // note update listener when click on note div.
-noteContent.addEventListener("click", (event) => {
-    event.preventDefault();
+const dataPassToForm = (currentElement) => {
+    let headingContent = currentElement.querySelector(".note-heading").textContent;
+    let paragraphContent = currentElement.querySelector(".note-paragraph").textContent;
     // pass the note content value to form filed.
-    getFormTitle.value = noteHeading.textContent;
-    getFormText.value =  noteParagraph.textContent;
-    // css customaization.
+    getFormTitle.value = headingContent;
+    getFormText.value =  paragraphContent;
+    // CSS customaization.
     formSaveBtn.style.display = "none";
     formSaveBtn.style.transition = "all .5s ease";
     formBtnGroup.style.display = "flex";
     formBtnGroup.style.transition = "all .5s ease";
-    // invoke event listener for note modification.
-    formUpdateBtn.addEventListener("click", updateNote)
-})
-
-// update event listener for note modification.
-let updateNote = (event) => {
-    event.preventDefault();
-        // pass the form filed to note content value.
-        noteHeading.textContent = getFormTitle.value;
-        noteParagraph.textContent = getFormText.value;
-        // invoke addnote function.
-    addNote(noteHeading.textContent, noteParagraph.textContent);
-}
-
-// shown the notes when window is loaded.
-let noteBinder = () => {
-    notes.forEach(note => {
-        notesWrapper.innerHTML += createNotetemp(note);
+    // update event listener for note modification.
+    formUpdateBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        headingContent = getFormTitle.value;
+        paragraphContent = getFormText.value;
+        formSaveBtn.style.display = "block";
+        formBtnGroup.style.display = "none";
+        console.log("fire!")
     });
 }
-// invoke notebinder function.
-noteBinder();
+// reset even listener for form clear.
+formResetBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    getFormTitle.value = "";
+    getFormText.value =  "";
+})
+// definition of closeNote for removing the note from the notes.
+const closeNote = (currentElement) => {
+    currentElement.parentNode.remove();
+}
+// shown the notes when window is loaded.
+(function notesShown() {
+    notes.forEach((note) => {
+        notesWrapper.innerHTML += createNotetemp(note);
+    });
+})();
